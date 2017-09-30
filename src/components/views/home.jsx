b1d40ed7dcd1ad62ecb4e011edc5a96b87694a61
Ethan Han <ethan.han47@gmail.com>
2017-09-30 17:11:47 -0400

    removing initial react test contents

--- src/components/views/home.jsx
+++ src/components/views/home.jsx
@@ -2,144 +2,45 @@ import React, { Component } from "react";
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
 
-function Square(props) {
-    return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    );
-}
-
-function checkWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
-}
-
-class Board extends React.Component {
-    renderSquare(i) {
+/*
+Navigation bar
+*/
+class NavBar extends React.Component {
+    render() {
         return (
-            <Square
-                value={this.props.squares[i]}
-                onClick={() => this.props.onClick(i)}
-            />
+            <div id="nav-bar">
+                <div className="main-icon">
+                    <img src="./" />
+                </div>
+            </div>
         );
+    }
+}
+
+/*
+Main contents
+*/
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                Home
+            </div>
+        );
+    }
+}
+
+export default class Home extends React.Component {
+    componentDidMount() {
+        browserHistory.push('/');
     }
 
     render() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                <NavBar />
+                <Content />
             </div>
         );
     }
-}
-
-class Game extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            history: [
-                {squares: Array(9).fill(null)} // first turn: all cells are empty/null
-            ],
-            xNext: true // X goes first
-        }
-    }
-
-    /*
-    handleClick(i) is sent as a callback to each individual square
-    this is a prop that can be accessed by Board, which passes it along to Square
-    */
-    handleClick(i) {
-        const history = this.state.history;
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
-
-        if (checkWinner(squares) || squares[i]) { // check if game was won
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            history: history.concat([{squares: squares}]),
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-
-    render() {
-        const history = this.state.history;
-        const current = history[history.length - 1];
-        const winner = checkWinner(current.squares);
-
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
-        const moves = history.map((step, move) => {
-            const desc = move ? 'Move #' + move : 'Game start';
-            return (
-                <li>
-                    <a href="" onClick={() => this.jumpTo(move)}>{desc}</a>
-                </li>
-            );
-        });
-
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board
-                        squares={current.squares}
-                        onClick={(i) => this.handleClick(i)}
-                    />
-                </div>
-                <div className="game-info">
-                    <div>{status}</div>
-                    <ol>{moves}</ol>
-                </div>
-            </div>
-        );
-    }
-}
-
-// ========================================
-
-export default class Home extends Component {
-  componentDidMount() {
-    browserHistory.push('/');
-  }
-  render() {
-    return (
-      <Game class="home" />
-    );
-  }
 }
