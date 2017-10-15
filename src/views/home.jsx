cc630eecc1924ea55685dfc10031f2817a8106cf
Ethan Han <ethan.han47@gmail.com>
2017-10-15 19:01:45 -0400

    removing a placeholder component

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -5,23 +5,6 @@ import NavigationBar from '../components/navigation_bar';
 import Jumbotron from '../components/jumbotron';
 import ContentMain from '../components/content_home';
 
-class Bookings extends React.Component {
-    render() {
-        return (
-            <div id="bookings">
-                Tour Dates
-                <div>
-                    Monday
-                    Tuesday
-                    Wednesday
-                    Thursday
-                    Friday
-                </div>
-            </div>
-        );
-    }
-}
-
 export default class Home extends React.Component {
     componentDidMount() {
         browserHistory.push('/');
