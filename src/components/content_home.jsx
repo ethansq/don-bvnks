2751595580f18619bf4d516de03a8ad67711177a
Ethan Han <ethan.han47@gmail.com>
2017-10-17 17:28:15 -0400

    added template for bookings list

--- src/components/content_home.jsx
+++ src/components/content_home.jsx
@@ -1,12 +1,7 @@
 import React, { Component } from "react";
 import ReactHtmlParser from 'react-html-parser';
 const contentJson = require('../content/main.json');
-
-function Callout(props) {
-	return(
-		<a className="callout-pill-wrapper" href={props.href}><div className="callout-pill">{props.name}</div></a>
-	);
-}
+const bookingsJson = contentJson.bookings;
 
 export default class ContentMain extends React.Component {
 	constructor() {
@@ -17,8 +12,28 @@ export default class ContentMain extends React.Component {
 	}
 
 	renderCallout(i, name, href) {
-		return <Callout key={i} href={href} name={name} />
+		return (
+            <a key={i} className="callout-pill-wrapper" href={href}><div className="callout-pill">{name}</div></a>
+        );
 	}
+
+    renderTourDate(i, tourDateJson) {
+        return (
+            <div key={i} className="tour-date-wrapper">
+                <span className="tour-name">{tourDateJson.name},&nbsp;</span>
+                <span className="tour-location">{tourDateJson.city+", "+tourDateJson.state},&nbsp;</span>
+                <span className="tour-date">{tourDateJson.date}</span>
+                <div className="tour-callouts">
+                    <a className="tour-callout" href={tourDateJson.moreInfo}>
+                        <div>TICKETS/INFO</div>
+                    </a>
+                    <a className="tour-callout" href={tourDateJson.moreInfo}>
+                        <div>VIP</div>
+                    </a>
+                </div>
+            </div>
+        );
+    }
 
 	innerHtml(str) {
 		return ReactHtmlParser(str);
@@ -36,17 +51,29 @@ export default class ContentMain extends React.Component {
 			);
     	}
 
+        var tourDates = [];
+        var numDates = bookingsJson.length;
+        for (var i=0; i<numDates; i++) {
+            tourDates[i] = this.renderTourDate(i, bookingsJson[i]);
+        }
+
         return (
         	<div className="home container">
-        		<h1 className="header">{contentJson.header}</h1>
-        		<h3 className="description">{this.innerHtml(contentJson.description)}</h3>
-        		<div className="callout-wrapper">
-        			<div className="label">STREAM LINKS</div>
-        			<div className="callouts">{callouts}</div>
-        		</div>
-	        	<div className="video">
-					<iframe src="https://www.youtube.com/embed/SkeCUHypDso?rel=0" frameBorder="0" allowFullScreen></iframe>
-	        	</div>
+                <section id="video-feature">
+            		<h1 className="header">{contentJson.header}</h1>
+            		<h3 className="description">{this.innerHtml(contentJson.description)}</h3>
+            		<div className="callout-wrapper">
+            			<div className="label">STREAM LINKS</div>
+            			<div className="callouts">{callouts}</div>
+            		</div>
+    	        	<div className="video">
+    					<iframe src="https://www.youtube.com/embed/SkeCUHypDso?rel=0" frameBorder="0" allowFullScreen></iframe>
+    	        	</div>
+                </section>
+                <section id="bookings">
+                    <h1 className="header">BOOKINGS</h1>
+                    <div className="bookings-list">{tourDates}</div>
+                </section>
         	</div>
         );
     }
